9e5506a4f677159f7502bdffc0c47594d22f4907
adekeyeyusuf520-commits <adekeyeyusuf520@gmail.com>
2026-05-16 18:36:57 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const locations = [];
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.post("/api/location", (req, res) => {
   const { latitude, longitude, accuracy, timestamp, label } = req.body;
 
